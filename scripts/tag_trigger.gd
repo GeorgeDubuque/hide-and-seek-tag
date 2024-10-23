1053bd3df4933ea2cb5e6b7f2f5e8aad73d8322b
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-10-22 22:37:06 -0700

    tagging stuff

--- scripts/tag_trigger.gd
+++ scripts/tag_trigger.gd
@@ -16,7 +16,7 @@ func _process(delta: float) -> void:
 	if !is_multiplayer_authority():
 		if Input.is_action_pressed(INTERACT) and last_taggee != null:
 			print("tag local: ", last_taggee)
-			check_can_tag.rpc_id(1, last_taggee)
+			check_can_tag.rpc(last_taggee)
 
 @rpc("any_peer", "call_local", "reliable")
 func check_can_tag(taggee_id):
