0f48b4bdf530f76a6ae84d810379c7c39cffa86a
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-10-22 23:32:10 -0700

    tagging stuff

--- FirstPersonController/addons/fpc/character.gd
+++ FirstPersonController/addons/fpc/character.gd
@@ -212,9 +212,8 @@ func _physics_process(delta):
 @rpc("any_peer", "call_local", "reliable")
 func freeze_player():
 	print("freeze player rpc called")
-	if is_multiplayer_authority():
-		is_frozen = true
-		print("is_frozen yay: ", is_frozen)
+	is_frozen = true
+	print("is_frozen yay: ", is_frozen)
 
 func handle_jumping():
 	if jumping_enabled:
