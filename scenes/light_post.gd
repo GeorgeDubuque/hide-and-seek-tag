89265fba9f68fd7183a489210a578c79c4a68cba
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-10-29 02:19:37 -0700

    debugging for multiplayer interaction logic

---
+++ scenes/light_post.gd
@@ -0,0 +1,12 @@
+extends Node3D
+@onready var interactionArea: InteractionArea = $InteractionArea
+@onready var light: OmniLight3D = $OmniLight3D
+var on: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	interactionArea.interact = Callable(self, "on_interact")
+
+func on_interact():
+	on = !on
+	light.visible = on
