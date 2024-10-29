2584b7c5edd69bda44ccf8257f2feaa97ac3d0b4
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-10-29 01:47:21 -0700

    debugging for multiplayer interaction logic

--- scripts/player_interactor.gd
+++ scripts/player_interactor.gd
@@ -32,24 +32,22 @@ func _input(event):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	# if multiplayer.is_server():
-	# 	print(player, "status can_interact=", can_interact, " and is_colliding()=", is_colliding())
-	if is_colliding():
+	if is_multiplayer_authority():
+		if is_colliding():
+			activeInteractable = get_collider()
+			# print(player, " looking at ", activeInteractable)
+		else:
+			activeInteractable = null
+			# print(player, " looking at nothing")
 
-		activeInteractable = get_collider()
-		# print(player, " looking at ", activeInteractable)
+		if activeInteractable != null:
+			InteractionManager.set_interaction_label_text(activeInteractable.actionName)
+			InteractionManager.label.show()
+		else:
+			InteractionManager.label.hide()
 
-		if multiplayer.is_server():
+	if multiplayer.is_server():
+		if is_colliding():
 			InteractionManager.register_area(get_collider(), player)
-	else:
-		activeInteractable = null
-		# print(player, " looking at nothing")
-
-		if multiplayer.is_server():
+		else:
 			InteractionManager.unregister_area(player)
-
-	if activeInteractable != null:
-		InteractionManager.set_interaction_label_text(activeInteractable.actionName)
-		InteractionManager.label.show()
-	else:
-		InteractionManager.label.hide()
