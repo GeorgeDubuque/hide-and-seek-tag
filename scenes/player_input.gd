018f32aa5c3bae2736d2ce258328c398034389c6
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-11-01 03:24:59 -0700

    never syncing player id

--- scenes/player_input.gd
+++ scenes/player_input.gd
@@ -18,10 +18,12 @@ extends MultiplayerSynchronizer
 @export var crouch_pressed := false
 @export var pause_button_just_pressed := false
 @export var mouse_input := Vector2()
+@export var label_username: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Only process for the local player.
+	label_username.text = Steam.getPersonaName()
 	set_process(get_multiplayer_authority() == multiplayer.get_unique_id())
 	set_process_unhandled_input(get_multiplayer_authority() == multiplayer.get_unique_id())
 
