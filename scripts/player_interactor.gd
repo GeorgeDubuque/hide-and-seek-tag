15b2919d9a033aca7632b0386bf0155ff64da88a
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-11-02 03:10:47 -0700

    textured and remodeled modular interior

--- scripts/player_interactor.gd
+++ scripts/player_interactor.gd
@@ -12,17 +12,6 @@ func _ready() -> void:
 	pass
 	# print(player)
 
-# func _input(event):
-# 	if !is_multiplayer_authority():
-# 		return false
-# 
-# 	if event.is_action_pressed("interact"):
-# 		if activeInteractable != null:
-# 			print(player, " with id ", player.multiplayer.get_unique_id(), " tried to interact with ", activeInteractable)
-# 			print(player, " client is sending rpc to server to check if interaction was valid")
-# 			InteractionManager.rpc_id(1, "is_valid_interact", activeInteractable.get_path())
-# 			InteractionManager.label.hide()
-
 func _physics_process(delta: float) -> void:
 	if !multiplayer.is_server():
 		return
@@ -57,23 +46,3 @@ func _process(_delta: float) -> void:
 	else:
 		interactLabel.hide()
 		# InteractionManager.label.hide()
-
-	# if is_multiplayer_authority():
-	# 	if is_colliding():
-	# 		activeInteractable = get_collider()
-	# 		# print(player, " looking at ", activeInteractable)
-	# 	else:
-	# 		activeInteractable = null
-	# 		# print(player, " looking at nothing")
-
-	# 	if activeInteractable != null:
-	# 		InteractionManager.set_interaction_label_text(activeInteractable.actionName)
-	# 		InteractionManager.label.show()
-	# 	else:
-	# 		InteractionManager.label.hide()
-
-	# if multiplayer.is_server():
-	# 	if is_colliding():
-	# 		InteractionManager.register_area(get_collider(), player)
-	# 	else:
-	# 		InteractionManager.unregister_area(player)
