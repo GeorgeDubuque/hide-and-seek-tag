c390660b253e48533ce4b9107c1b501f10539ce6
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-11-05 01:44:57 -0800

    adding items

--- scenes/player_input.gd
+++ scenes/player_input.gd
@@ -45,6 +45,9 @@ func jump():
 func interact():
 	interact_button_just_pressed = true
 
+@rpc("call_local")
+func use_item():
+	use_item_button_just_pressed = true
 
 @rpc("call_local")
 func move_head(mouse_movement: Vector2):
@@ -71,7 +74,9 @@ func _process(delta: float) -> void:
 	sprinting_pressed = Input.is_action_pressed(SPRINT)
 	crouch_just_pressed = Input.is_action_just_pressed(CROUCH)
 	crouch_pressed = Input.is_action_pressed(CROUCH)
-	use_item_button_just_pressed = Input.is_action_just_pressed(FLASHLIGHT)
+
+	if Input.is_action_just_pressed(FLASHLIGHT):
+		use_item.rpc()
 
 	if Input.is_action_just_pressed(INTERACT):
 		interact.rpc()
