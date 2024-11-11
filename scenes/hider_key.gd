546a9203f4a2afd7504a29092485055ef279e822
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-11-11 12:19:09 -0800

    got spawning keys working; remember to spawn objects under spawn path node and nowhere else

--- scenes/hider_key.gd
+++ scenes/hider_key.gd
@@ -48,3 +48,6 @@ func enableKey(player_id):
 	# meshColor.a = 0.5
 	# material.albedo_color = meshColor
 	# mesh.set_surface_override_material(0, material)
+
+func _process(delta):
+	print("is key disabled: ", $InteractionArea/CollisionShape3D.disabled)
