1d04cb0f0bdcc66dcee8701d08b895002b056b38
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-11-11 10:34:10 -0800

    got spawning keys working; remember to spawn objects under spawn path node and nowhere else

--- scenes/hider_key.gd
+++ scenes/hider_key.gd
@@ -20,7 +20,7 @@ var hiderKeyRes: HiderKeyRes:
 func _ready() -> void:
 	disableKey()
 
-@rpc("call_local", "any_peer")
+@rpc("call_local", "any_peer", "reliable")
 func enableKey():
 	print("enabling key on ", multiplayer.get_unique_id())
 	interactionArea.enable()
