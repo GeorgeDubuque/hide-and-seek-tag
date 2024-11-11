7b5055ac519e4aef8b73184d9596a794e20fe9ad
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-11-11 09:33:18 -0800

    got spawning keys working; remember to spawn objects under spawn path node and nowhere else

--- scenes/hider_key.gd
+++ scenes/hider_key.gd
@@ -25,17 +25,17 @@ func enableKey():
 	print("enabling key on ", multiplayer.get_unique_id())
 	interactionArea.enable()
 	mesh.transparency = 0.0
-	var material: Material = hiderKeyRes.material.duplicate()
-	var meshColor: Color = material.albedo_color
-	meshColor.a = 1.0
-	material.albedo_color = meshColor
-	mesh.set_surface_override_material(0, material)
+	# var material: Material = hiderKeyRes.material.duplicate()
+	# var meshColor: Color = material.albedo_color
+	# meshColor.a = 1.0
+	# material.albedo_color = meshColor
+	# mesh.set_surface_override_material(0, material)
 
 func disableKey():
 	interactionArea.disable()
 	mesh.transparency = 0.8
-	var material: Material = hiderKeyRes.material.duplicate()
-	var meshColor: Color = material.albedo_color
-	meshColor.a = 0.5
-	material.albedo_color = meshColor
-	mesh.set_surface_override_material(0, material)
+	# var material: Material = hiderKeyRes.material.duplicate()
+	# var meshColor: Color = material.albedo_color
+	# meshColor.a = 0.5
+	# material.albedo_color = meshColor
+	# mesh.set_surface_override_material(0, material)
