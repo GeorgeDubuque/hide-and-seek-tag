056af0143189c517eb1700c5008cfefda1f69401
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-11-11 12:04:13 -0800

    got spawning keys working; remember to spawn objects under spawn path node and nowhere else

--- scripts/player.gd
+++ scripts/player.gd
@@ -49,6 +49,7 @@ extends CharacterBody3D
 var hiderKeyRes: HiderKeyRes:
 	set(value):
 		hiderKeyRes = value
+		print("setting hider color on: ", multiplayer.get_unique_id())
 		playerBodyMesh.material_override = hiderKeyRes.material
 
 
