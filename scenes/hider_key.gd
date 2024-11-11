79284268d843c4baa4bc9c3533a95382db229c11
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-11-11 12:16:44 -0800

    got spawning keys working; remember to spawn objects under spawn path node and nowhere else

--- scenes/hider_key.gd
+++ scenes/hider_key.gd
@@ -25,13 +25,15 @@ func _ready() -> void:
 func enableKey(player_id):
 	if (multiplayer.get_unique_id() == player_id):
 		print("ENABLING key on ", multiplayer.get_unique_id())
+		print("disabling key collider with path ", $InteractionArea/CollisionShape3D.get_path())
 		# interactionArea.enable()
 		$InteractionArea/CollisionShape3D.disabled = false
 		mesh.transparency = 0.0
 	else:
 		print("DISABLING key on ", multiplayer.get_unique_id())
 		# interactionArea.disable()
-		$InteractionArea/CollisionShape3D.set_disabled(true)
+		print("disabling key collider with path ", $InteractionArea/CollisionShape3D.get_path())
+		$InteractionArea/CollisionShape3D.disabled = true
 		mesh.transparency = 0.8
 	# var material: Material = hiderKeyRes.material.duplicate()
 	# var meshColor: Color = material.albedo_color
